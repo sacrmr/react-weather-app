73f4b986d4c77c4c79544674f241fed69a3bed10
sacrmr <106908810+sacrmr@users.noreply.github.com>
2022-12-20 12:14:43 +0100

    Added the Github repo Url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        <a
+          className="info-link"
+          href="https://github.com/sacrmr/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code {""}
+        </a>
+        on Github by Samantha Cramer
+      </footer>
+    </div>
+  );
 }
 
 export default App;
