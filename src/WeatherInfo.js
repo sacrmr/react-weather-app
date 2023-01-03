9e52af09b76b9489079f10c8902eefaa92630948
sacrmr <106908810+sacrmr@users.noreply.github.com>
2023-01-03 09:57:59 +0100

    corrected delete of Fahrenheit

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,7 +2,6 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
-
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
